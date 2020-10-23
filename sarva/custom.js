e1e190e79e6320fc01a985e8a0aefa0670aa973c
Taryn Gloyne <TarynG@saeon.local>
2020-10-23 16:27:11 +0200

    home display

--- sarva/custom.js
+++ sarva/custom.js
@@ -15,5 +15,40 @@ jQuery(document).ready(function($) {
       //var searchurl = 'https://catalogue.saeon.ac.za/records?terms=' 
 		console.log(searchurl + searchterm);
 		window.open(searchurl + searchterm,'_blank');
-	});
+   });
+   
+   /* Home Display */
+   
+
+   window.addEventListener('load', function(){
+
+      thbg = $('.swiper-slide-bg');
+      console.log(thbg);
+
+      $('.homeds').find('.swiper-slide-bg').each(function() {
+
+         // thbg = $(this).css('background-image');
+         // console.log(thbg);
+         // $.each(this.attributes, function(i, attrib){
+         //    var name = attrib.name;
+         //    var value = attrib.value;
+         //    // do your magic :-)
+         // });
+       });
+
+
+   //    var hm_icons = $('.homeds').find('.swiper-slide-bg');
+
+   //    $(hm_icons).each(function(){
+   //       console.log($(this).css('background-image'));
+   //   })
+   
+
+      // hm_icons.each(function(){ 
+      //    var itsbg = $(this).css('background-image');
+      //    hm_icons = hm_icons.replace('url(','').replace(')','').replace(/\"/gi, "");
+      //    console.log(hm_icons);
+      // });
+
+    });
 });
